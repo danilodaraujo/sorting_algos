fd516f3d88683913a11f259143f27cae4f421197
Danilo <danilodaraujo@gmail.com>
2024-02-19 20:21:14 +0000

    Initial Commit

---
+++ sorting_algos/sorter.py
@@ -0,0 +1,21 @@
+class Sorter:
+    def __init__(self):
+        pass
+    
+    def Quick(self):
+        pass
+    
+    def Insertion(self):
+        pass
+    
+    def Bubble(self):
+        pass
+    
+    def Selection(self):
+        pass
+    
+    def Merge(self):
+        pass
+    
+    def Heap(self):
+        pass
---
+++ sorting_algos/starter_data.py
@@ -0,0 +1,15 @@
+class StarterData:
+    def __init__(self, size):
+        self.size = size
+        
+    def Random(self):
+        pass
+        
+    def Stratified(self):
+        pass
+        
+    def Reversed(self):
+        pass
+        
+    def AlmostSorted(self):
+        pass
